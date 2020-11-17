626e83f68007b0120e5559a92bbdf2b256c609b8
Dennis Boeckmann <dev@dbck.de>
2020-11-17 20:55:23 +0100

    Completed editing contacts form with actions

---
+++ src/main/java/de/dbck/poc/pocspringvaadin/ui/MainView.java
@@ -0,0 +1,118 @@
+package de.dbck.poc.pocspringvaadin.ui;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.Route;
+import de.dbck.poc.pocspringvaadin.backend.entity.Company;
+import de.dbck.poc.pocspringvaadin.backend.entity.Contact;
+import de.dbck.poc.pocspringvaadin.backend.service.CompanyService;
+import de.dbck.poc.pocspringvaadin.backend.service.ContactService;
+
+@Route("")
+@CssImport("./styles/shared-styles.css")
+// @Theme(value=Material.class, variant = Material.DARK)
+// @Theme(value=Lumo.class, variant = Lumo.LIGHT)
+public class MainView extends VerticalLayout {
+  private static final long serialVersionUID = 1L;
+
+  private ContactService contactService;
+
+  private Grid<Contact> grid = new Grid<>(Contact.class);
+  private TextField filterText = new TextField();
+  private ContactForm form;
+
+  public MainView(ContactService contactService, CompanyService companyService) {
+    this.contactService = contactService;
+    addClassName("list-view");
+    setSizeFull();
+
+    configureGrid();
+
+
+    form = new ContactForm(companyService.findAll());
+    form.addListener(ContactForm.SaveEvent.class, this::saveContact);
+    form.addListener(ContactForm.DeleteEvent.class, this::deleteContact);
+    form.addListener(ContactForm.CloseEvent.class, e -> closeEditor());
+
+    Div content = new Div(grid, form);
+    content.addClassName("content");
+    content.setSizeFull();
+
+    add(getToolbar(), content);
+    updateList();
+    closeEditor();
+  }
+
+  private void configureGrid() {
+    grid.addClassName("contact-grid");
+    grid.setSizeFull();
+    grid.removeColumnByKey("company");
+    grid.setColumns("firstName", "lastName", "email", "status");
+    grid.addColumn(contact -> {
+      Company company = contact.getCompany();
+      return company == null ? "-" : company.getName();
+    }).setHeader("Company");
+    grid.getColumns().forEach(col -> col.setAutoWidth(true));
+    grid.asSingleSelect().addValueChangeListener(event -> editContact(event.getValue()));
+  }
+
+  private void updateList() {
+    grid.setItems(contactService.findAll(filterText.getValue()));
+  }
+
+
+
+  private void closeEditor() {
+    form.setContact(null);
+    form.setVisible(false);
+    removeClassName("editing");
+  }
+
+  void addContact() {
+    grid.asSingleSelect().clear();
+    editContact(new Contact());
+  }
+  
+  public void editContact(Contact contact) {
+    if (contact == null) {
+      closeEditor();
+    } else {
+      form.setContact(contact);
+      form.setVisible(true);
+      addClassName("editing");
+    }
+  }
+  
+  private void saveContact(ContactForm.SaveEvent event) {
+    contactService.save(event.getContact());
+    updateList();
+    closeEditor();
+  }
+
+  private void deleteContact(ContactForm.DeleteEvent event) {
+    contactService.delete(event.getContact());
+    updateList();
+    closeEditor();
+  }
+
+  private HorizontalLayout getToolbar() {
+    filterText.setPlaceholder("Filter by name...");
+    filterText.setClearButtonVisible(true);
+    filterText.setValueChangeMode(ValueChangeMode.LAZY);
+    filterText.addValueChangeListener(e -> updateList());
+
+    Button addContactButton = new Button("Add contact");
+    addContactButton.addClickListener(click -> addContact());
+
+    HorizontalLayout toolbar = new HorizontalLayout(filterText, addContactButton);
+    toolbar.addClassName("toolbar");
+    return toolbar;
+  }
+}
+
